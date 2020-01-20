d3b361d7a6c900f3ab919815cf02e2cd61aadfff
Joaquin Umpierrez <jo.umpierrez@gmail.com>
2020-01-20 15:38:26 -0300

    Agregado roles a los usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_request, only: %i[login register]
+  skip_before_action :authenticate_request, only: %i[login register addRole]
 
   # Metodo para registrar un usuario
   def register
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-=begin
+
 ######  Roles  ######
 roles_list = %w(Administrador Delivery)
 
 roles_list.each do |name|
   Role.create(name: name)
 end
-=end
+
 ##### Empresas #####
 merchants_list = [
     ["171717171", "Empresa Integrador Principal"],
@@ -31,9 +31,12 @@ end
 
 ###### Usuarios #####
 users_list = [
-    ["pablo@test.com", "pablo", "Pablo", "Diaz", "099999999", "CI", "1111111-1", 1],
-    ["gaston@test.com", "gaston", "Gaston", "Cabana", "088888888", "CI", "2222222-2", 1],
-    ["joaquin@test.com", "joaquin", "Joaquin", "Umpierrez", "077777777", "CI", "3333333-3", 1]
+    ["pablo@admin.com", "pablo", "Pablo", "Diaz", "099999999", "CI", "1111111-1", 1],
+    ["gaston@admin.com", "gaston", "Gaston", "Cabana", "088888888", "CI", "2222222-2", 1],
+    ["joaquin@admin.com", "joaquin", "Joaquin", "Umpierrez", "077777777", "CI", "3333333-3", 1],
+    ["pablo@delivery.com", "pablo", "Pablo", "Diaz", "099999999", "CI", "1111111-1", 1],
+    ["gaston@delivery.com", "gaston", "Gaston", "Cabana", "088888888", "CI", "2222222-2", 1],
+    ["joaquin@delivery.com", "joaquin", "Joaquin", "Umpierrez", "077777777", "CI", "3333333-3", 1]
 ]
 
 users_list.each do |email, password, name, lastname, phone, document_type, document_number, merchant_id|
@@ -42,3 +45,11 @@ users_list.each do |email, password, name, lastname, phone, document_type, docum
 end
 
 ### Roles_usuario ###
+# Id 1 es Administrador
+# Id 2 es Delivery
+User.find(1).add_role(:administrador)
+User.find(2).add_role(:administrador)
+User.find(3).add_role(:administrador)
+User.find(4).add_role(:delivery)
+User.find(5).add_role(:delivery)
+User.find(6).add_role(:delivery)
