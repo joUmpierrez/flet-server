0dcb9ab930956834a2894ebd0e1ac9de79c945fb
Joaquin Umpierrez <jo.umpierrez@gmail.com>
2020-03-10 02:02:23 -0300

    fixing login

---
+++ lib/json_web_token.rb
@@ -0,0 +1,23 @@
+class JsonWebToken
+  class << self
+    def encode(payload, exp = 2.hours.from_now)
+      # Tiempo de expiracion (2 horas)
+      payload[:exp] = exp.to_i
+
+      # Codificado
+      JWT.encode(payload, Rails.application.credentials.dig(:secret_key_base))
+    end
+
+    def decode(token)
+      # Decodificado
+      body = JWT.decode(token, Rails.application.credentials.dig(:secret_key_base))[0]
+      HashWithIndifferentAccess.new body
+
+        # Errores personalizados
+    rescue JWT::ExpiredSignature, JWT::VerificationError => e
+      raise ExceptionHandler::ExpiredSignature, e.message
+    rescue JWT::DecodeError, JWT::VerificationError => e
+      raise ExceptionHandler::DecodeError, e.message
+    end
+  end
+end
